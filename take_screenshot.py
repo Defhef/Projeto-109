1aa6100b8c3a0a09ace41f621f5ae9ca5640f633
Defhef <105176513+Defhef@users.noreply.github.com>
2023-06-06 19:09:13 +0300

    Add files via upload

--- take_screenshot.py
+++ take_screenshot.py
@@ -48,15 +48,15 @@ while True:
             if all(finger_fold_status):
                 
                 # ESCREVA O CÓDIGO AQUI
-             image = pyautogui.screenshot()  
-            pyautogui.screenshot("straight_to_disk.png")
-            image.imread("straight_to_disk.png")
-            cv2.imwrite("in_memory_to_disk.png",image)
-                
-            image = pyautogui.screenshot()
-            image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR).cv2.imwrite("in_memory_to_disk.png", image)
+                image = pyautogui.screenshot()  
+                image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+                cv2.imwrite("in_memory_to_disk.png",image)
+                pyautogui.screenshot("straight_to_disk.png")
+                image.imread("straight_to_disk.png")    
 
-            cv2.imshow("Captura de tela", imutils.resize(image, width=600))
+                image = cv2.imread("straight_to_disk.png")      
+
+                cv2.imshow("Captura de tela", imutils.resize(image, width=600))
             
             mp_draw.draw_landmarks(img, hand_landmark,
             mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
